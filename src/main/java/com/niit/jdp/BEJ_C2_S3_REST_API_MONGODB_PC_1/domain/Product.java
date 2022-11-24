9d53dad8a2b711fe29ca19a423f57d669b200546
M V K <krishna.mugala9@gmail.com>
2022-11-24 15:32:58 +0530

    created getter setters in product class

--- src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_PC_1/domain/Product.java
+++ src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_PC_1/domain/Product.java
@@ -21,4 +21,28 @@ public class Product {
         this.productName = productName;
         this.description = description;
     }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
