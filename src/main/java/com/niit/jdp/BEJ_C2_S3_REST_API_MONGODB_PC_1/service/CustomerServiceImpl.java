d3c1d8bfcf7cdd18c274fa7a6f5b53698c2f4599
M V K <krishna.mugala9@gmail.com>
2022-11-24 16:09:13 +0530

    implemented all methods in impl class

--- src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_PC_1/service/CustomerServiceImpl.java
+++ src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_PC_1/service/CustomerServiceImpl.java
@@ -8,5 +8,33 @@
 
 package com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_PC_1.service;
 
-public class CustomerServiceImpl {
+import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_PC_1.domain.Customer;
+
+import java.util.List;
+
+public class CustomerServiceImpl implements ICustomerService {
+    @Override
+    public Customer saveCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> getAllCustomer() {
+        return null;
+    }
+
+    @Override
+    public List<Customer> getAllCustomersByName(String customerName) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> findAllCustomerProductName(String productName) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteCustomerById(int customerId) {
+        return false;
+    }
 }
