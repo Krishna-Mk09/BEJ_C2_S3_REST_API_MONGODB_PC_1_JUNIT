2dc75af59180cd80248e25b9b9be9f6d7a7f1af3
M V K <krishna.mugala9@gmail.com>
2022-11-24 15:30:44 +0530

    formatted the customer class

--- src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_PC_1/domain/Customer.java
+++ src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_PC_1/domain/Customer.java
@@ -58,11 +58,6 @@ public class Customer {
 
     @Override
     public String toString() {
-        return "Customer{" +
-                "customerId=" + customerId +
-                ", customerName='" + customerName + '\'' +
-                ", PhoneNumber=" + PhoneNumber +
-                ", customerProduct=" + customerProduct +
-                '}';
+        return "Customer{" + "customerId=" + customerId + ", customerName='" + customerName + '\'' + ", PhoneNumber=" + PhoneNumber + ", customerProduct=" + customerProduct + '}';
     }
 }
