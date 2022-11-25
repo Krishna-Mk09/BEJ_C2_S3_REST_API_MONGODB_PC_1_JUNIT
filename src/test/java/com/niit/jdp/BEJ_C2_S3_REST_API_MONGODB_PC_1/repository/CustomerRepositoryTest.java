fecc73da89524a526457600879fbb81ff558d1ee
M V K <krishna.mugala9@gmail.com>
2022-11-25 12:28:19 +0530

    added doc writers in the test folders

--- src/test/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_PC_1/repository/CustomerRepositoryTest.java
+++ src/test/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_PC_1/repository/CustomerRepositoryTest.java
@@ -24,12 +24,18 @@ class CustomerRepositoryTest {
     private Product customerProduct;
     private Product product1, product2;
 
+    /**
+     * A setup function which is called before each test case.
+     */
     @BeforeEach
     void setUp() {
         product1 = new Product(111, "mac", "my mac");
         customer = new Customer(222, "vamshi", 873472342, product1);
     }
 
+    /**
+     * The function deletes all the data in the database after each test
+     */
     @AfterEach
     void tearDown() {
         product1 = null;
@@ -37,36 +43,54 @@ class CustomerRepositoryTest {
         customerRepository.deleteAll();
     }
 
+    /**
+     * This function is used to test the customer name
+     */
     @Test
     void findAllCustomerName() {
         assertEquals("vamshi", customer.getCustomerName());
         assertNotEquals(112, customer.getCustomerName());
     }
 
+    /**
+     * This function tests the getProductName() function in the Product class
+     */
     @Test
     void getCustomerProductName() {
         assertEquals("mac", product1.getProductName());
         assertNotEquals("MAC", product1.getProductName());
     }
 
+    /**
+     * This function tests the getProductId() function in the Product class
+     */
     @Test
     void getProductById() {
         assertEquals(111, product1.getProductId());
         assertNotEquals(11.1, product1.getProductId());
     }
 
+    /**
+     * The function getCustomerById() tests that the customerId is 222 and not 1.22
+     */
     @Test
     void getCustomerById() {
         assertEquals(222, customer.getCustomerId());
         assertNotEquals(1.22, customer.getCustomerId());
     }
 
+    /**
+     * This function tests the getPhoneNumber() function in the Customer class
+     */
     @Test
     void getCustomerByPhone() {
         assertEquals(873472342, customer.getPhoneNumber());
         assertNotEquals(87347234, customer.getPhoneNumber());
     }
 
+    /**
+     * The function deletes a customer from the database and then checks if the customer is still in the database
+     */
     @Test
     void delete() {
         Customer c1 = customerRepository.save(customer);
